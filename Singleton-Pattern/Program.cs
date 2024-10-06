2096bbbf58133c80177df301f53a27e94e019deb
kapoorkhushal <khushalkapoor@IN-910D5S3>
2024-10-06 23:32:19 +0530

    Add project files.

---
+++ Singleton-Pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using Singleton_Pattern;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // To demonstrate thread safety
+            Parallel.Invoke(
+                () => Singleton.GetInstance.PrintMessage("Object 1"),
+                () => Singleton.GetInstance.PrintMessage("Object 2")
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton-Pattern/Singleton.cs
@@ -0,0 +1,40 @@
+﻿namespace Singleton_Pattern
+{
+    internal sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton? instance = null;
+        private static readonly object lockObject = new object();
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Inside Singleton class constructor");
+            Console.WriteLine($"counter: {counter}");
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                // This is double-checked locking
+                if (instance is null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance is null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void PrintMessage(string str)
+        {
+            Console.WriteLine($"Message: {str}");
+        }
+    }
+}
